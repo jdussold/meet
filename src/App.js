061d1b6669500a3b722000d290d4e2a978ccd38f
jdussold <jdussold@gmail.com>
2023-02-17 11:32:55 -0600

    updated to not show welcome screen when API data is fetched

--- src/App.js
+++ src/App.js
@@ -54,7 +54,11 @@ class App extends Component {
     if (navigator.onLine && (code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
+          this.setState({
+            events,
+            locations: extractLocations(events),
+            showWelcomeScreen: false,
+          });
         }
       });
     }
