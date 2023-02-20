3dc770c8608d29cb9ba815457390aadbd54e738d
jdussold <jdussold@gmail.com>
2023-02-20 16:12:20 -0600

    Updated with comments explaining each part of the code

--- src/EventList.js
+++ src/EventList.js
@@ -1,10 +1,14 @@
+// Import required packages and components
 import React, { Component } from "react";
 import Event from "./Event";
 
+// Define the EventList component as a class component
 class EventList extends Component {
   render() {
+    // Destructure the "events" and "numberOfEvents" props from the component's props object
     const { events } = this.props;
     const displayedEvents = events.slice(0, this.props.numberOfEvents);
+    // Render a list of "Event" components, one for each event in the "displayedEvents" array
     return (
       <ul className="EventList">
         {displayedEvents.map((event) => (
@@ -17,4 +21,5 @@ class EventList extends Component {
   }
 }
 
+// Export the "EventList" component as the default export
 export default EventList;
