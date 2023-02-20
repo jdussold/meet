4e92260f7c0bd1479bbea56fb3dd9347bd3ea842
jdussold <jdussold@gmail.com>
2023-02-20 01:24:59 -0600

    Revert "updated with regex"
    This reverts commit 423476323e5b12c7ede949f18305da7017cf454d.

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,12 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  PieChart,
-  Pie,
-  Cell,
-  ResponsiveContainer,
-  Legend,
-  XAxis,
-} from "recharts";
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from "recharts";
 
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
@@ -14,18 +7,24 @@ const EventGenre = ({ events }) => {
 
   useEffect(() => {
     const getData = () => {
-      const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
-      const data = genres.map((genre, index) => {
-        const regex = new RegExp(`${genre}(\\(.+\\))?(-Remote)?`, "i");
+      const genres = [
+        "React",
+        "JavaScript",
+        "Node(.js)?",
+        "jQuery",
+        "Angular(JS)?(-Remote)?",
+      ];
+      const data = genres.map((genre) => {
+        const regex = new RegExp(genre, "i");
         const value = events.filter((event) =>
           regex.test(event.summary)
         ).length;
-        return { name: genres[index], value };
+        return { name: genre, value };
       });
       console.log("Data:", data);
       return data;
     };
-    setData(getData());
+    setData(() => getData());
   }, [events]);
 
   return (
@@ -45,7 +44,6 @@ const EventGenre = ({ events }) => {
             <Cell key={`cell-${index}`} fill={colors[index]} />
           ))}
         </Pie>
-        <XAxis dataKey="name" />
         <Legend verticalAlign="bottom" height={50} />
       </PieChart>
     </ResponsiveContainer>
