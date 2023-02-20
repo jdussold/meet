6487a01d3673bd2110a177fdaedcc87017455ca1
jdussold <jdussold@gmail.com>
2023-02-20 14:31:00 -0600

    updated with comments explaining code

--- src/api.js
+++ src/api.js
@@ -1,21 +1,18 @@
-/**
- *
- * @param {*} events:
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
-
+// Importing required dependencies
 import { mockData } from "./mock-data";
 import axios from "axios";
 import NProgress from "nprogress";
 
+// This function takes an events array, then uses map to create a new array with only locations.
+// It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+// The Set will remove all duplicates from the array.
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;
 };
 
+// This function checks if the provided access token is valid by making a GET request to the Google OAuth API.
 export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
@@ -26,6 +23,7 @@ export const checkToken = async (accessToken) => {
   return result;
 };
 
+// This function removes any query parameters from the current URL, which may have been added during the OAuth authentication process.
 const removeQuery = () => {
   if (window.history.pushState && window.location.pathname) {
     var newurl =
@@ -40,6 +38,9 @@ const removeQuery = () => {
   }
 };
 
+// This function exchanges the authorization code obtained during the OAuth authentication process for an access token.
+// The function makes a GET request to a custom backend API that is responsible for exchanging the code for an access token.
+// If an access token is obtained, it is saved to localStorage and returned. If an error occurs, the error is returned.
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
@@ -57,6 +58,10 @@ const getToken = async (code) => {
   return access_token;
 };
 
+// This function retrieves the access token from localStorage and checks if it is still valid.
+// If the access token is not valid or not present, the function will attempt to exchange the authorization code for a new access token.
+// If the authorization code is not present, the function will redirect the user to the Google OAuth authorization page.
+// The function returns the access token if it is valid and present, or the newly obtained access token if the code was exchanged successfully.
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem("access_token");
   const tokenCheck = accessToken && (await checkToken(accessToken));
@@ -77,6 +82,10 @@ export const getAccessToken = async () => {
   return accessToken;
 };
 
+// This function retrieves a list of events using the provided access token.
+// If the user is running the app locally, the function returns mock data instead of making a network request.
+// If the user is offline, the function retrieves the last stored events from localStorage and returns them.
+// If the user is online and has a valid access token, the function retrieves a list of events from the backend API and returns them.
 export const getEvents = async () => {
   NProgress.start();
 
